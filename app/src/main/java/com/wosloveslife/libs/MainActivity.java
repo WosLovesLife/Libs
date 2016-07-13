267d1aa05eff103c64b99237f2e80d6da4d746a7
WosLovesLife <zhang00h00@126.com>
2016-07-13 16:09:45 +0800

    手指触摸在轮播图上时暂停轮播

--- app/src/main/java/com/wosloveslife/libs/MainActivity.java
+++ app/src/main/java/com/wosloveslife/libs/MainActivity.java
@@ -1,13 +1,71 @@
 package com.wosloveslife.libs;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.wosloveslife.loopviewpager.adapter.LoopViewPagerAdapter;
+import com.wosloveslife.loopviewpager.view.LoopViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoopViewPager mLoopViewPager;
+    private LoopViewPager mLoopViewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        /* 模拟数据 */
+        List<Bitmap> data = new ArrayList<>();
+        data.add(BitmapFactory.decodeResource(getResources(), R.drawable.icon1));
+        data.add(BitmapFactory.decodeResource(getResources(), R.drawable.icon2));
+        data.add(BitmapFactory.decodeResource(getResources(), R.drawable.icon3));
+        data.add(BitmapFactory.decodeResource(getResources(), R.drawable.icon4));
+        data.add(BitmapFactory.decodeResource(getResources(), R.drawable.icon5));
+
+        mLoopViewPager = (LoopViewPager) findViewById(R.id.id_vp_loop_view_pager);
+        mLoopViewPager.setAdapter(new MyLoopAdapter(data));
+        mLoopViewPager.startLoop();
+
+        mLoopViewPager2 = (LoopViewPager) findViewById(R.id.id_vp_loop_view_pager2);
+        mLoopViewPager2.setAdapter(new MyLoopAdapter(data));
+        mLoopViewPager2.setDuration(2500);
+        mLoopViewPager2.startLoop();
+    }
+
+    class MyLoopAdapter extends LoopViewPagerAdapter<Bitmap> {
+
+        public MyLoopAdapter(List<Bitmap> data) {
+            super(data);
+        }
+
+        @Override
+        protected View setView(ViewGroup container, int position) {
+            final ImageView imageView = new ImageView(container.getContext());
+            imageView.setImageBitmap(mData.get(position));
+            return imageView;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mLoopViewPager.stopLoop();
+        mLoopViewPager2.stopLoop();
     }
 }
